19feffc38245bd0c42ab509b559875d4899696d2
neferpitou <mdavila97@gmail.com>
2014-02-06 21:00:22 -0500

    More Activities Created
    Created and extended a GameSettingsActivity so the user can alter some
    settings before beginning a game, and a placeholder SheepHerderActivity
    to represent the activity that will hold the game. The GSA has two
    buttons to move back to the main screen and forward to the SHA.

---
+++ src/edu/lehman/android/GameSettingsActivity.java
@@ -0,0 +1,58 @@
+package edu.lehman.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+/**
+ * A pre-game settings menu where user can alter difficulty and other parameters.
+ * 
+ * @author Marcos Davila
+ * @revisionhistory
+ * 		2/6/2014 - Back and start buttons now move to the activities
+ * 		2/4/2014 - File created
+ */
+public class GameSettingsActivity extends Activity {
+
+	// Intents for the buttons to point to the class files of the activities
+	// that they should launch
+	private Intent backIntent;
+	private Intent startGameIntent;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_settings_panel);
+		
+		backIntent = new Intent(this, MainActivity.class);
+		startGameIntent = new Intent(this, SheepHerderActivity.class);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.game_settings_panel, menu);
+		return true;
+	}
+	
+	/*
+	 * When the back button is clicked, launch the activity associated
+	 * with the intent to return to the start menu. The onClick listener
+	 * is defined in the xml for this activity
+	 */
+	public void goBack(View view){
+		startActivity(backIntent);
+	}
+	
+	/*
+	 * When the start button is clicked, launch the activity associated
+	 * with starting the game. The onClicklistener is defined in the xml
+	 * for this activity
+	 */
+	public void startGame(View view){
+		startActivity(startGameIntent);
+	}
+
+}
